ab15453f33159738af542d6f5fa19c1c11e07657
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-09-08 21:15:21 +0530

    Time: 0 ms (100%), Space: 40.7 MB (58.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,11 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
+        return Math.pow(x, n);
+        /*
         if(n < 0){
             return helper(1 / x, Math.abs((long) n));
         }else{
             return helper(x, n);
         }
+        */
     }
+    /*
     private double helper(double x, long n){
         if(n == 0){
             return 1.0;
@@ -19,4 +23,5 @@ class Solution {
             return x * helper(x, n - 1);
         }
     }
+    */
 }
