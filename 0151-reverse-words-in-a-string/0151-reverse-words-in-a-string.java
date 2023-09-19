075d78bf8eaba156def425eeaf66dfb440c2c4c7
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-09-19 17:24:59 +0530

    Time: 9 ms (49.68%), Space: 41.7 MB (89.2%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,11 +3,16 @@ class Solution {
         if(s == null){
             return s;
         }
+        /*
         String[] str = s.trim().split("\\s+");
         String out = "";
         for(int i = str.length - 1; i > 0; i--){
             out += str[i] + " ";
         }
         return out + str[0];
+        */
+        String words[] = s.trim().split("\\s+");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
     }
 }
