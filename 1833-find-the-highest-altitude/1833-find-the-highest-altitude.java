55a88c5f1b03d5c41649e2bebe1b982449f33221
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-09-20 17:18:23 +0530

    Time: 0 ms (100%), Space: 40 MB (60.39%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -1,5 +1,6 @@
 class Solution {
     public int largestAltitude(int[] gain) {
+        /*
         int[] arr = new int[gain.length + 1];
         arr[0] = 0;
         for(int i = 0; i < gain.length; i++){
@@ -7,5 +8,12 @@ class Solution {
         }
         Arrays.sort(arr);
         return arr[gain.length];
+        */
+        int currentAlt = 0, maxAlt = 0;
+        for(int i = 0; i < gain.length; i++){
+            currentAlt += gain[i];
+            maxAlt = Math.max(currentAlt, maxAlt);
+        }
+        return maxAlt;
     }
 }
