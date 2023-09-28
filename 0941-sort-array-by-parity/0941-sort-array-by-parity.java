2b4872fb006d79454dc6402584f0d64bab63b850
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-09-28 11:15:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (83.97%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
+        /*
         int j = 0;
         int k = nums.length - 1;
         int result[] = new int[nums.length];
@@ -13,5 +14,18 @@ class Solution {
             }
         }
         return result;
+        */
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0){
+                swap(nums, i, j++);
+            }
+        }
+        return nums;
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
