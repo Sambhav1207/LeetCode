7002b74ed4b092f6e8e8c174d787f40a094492ac
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-10-06 09:58:14 +0530

    Time: 0 ms (100%), Space: 39.4 MB (21.28%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,5 +1,6 @@
 class Solution {
     public int integerBreak(int n) {
+        /*
         if(n <= 1){
             return 0;
         }
@@ -11,5 +12,19 @@ class Solution {
             }
         }
         return dp[n];
+        */
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int prod = 1;
+        while(n > 4){
+            prod *= 3;
+            n -= 3;
+        }
+        prod *= n;
+        return prod;
     }
 }
