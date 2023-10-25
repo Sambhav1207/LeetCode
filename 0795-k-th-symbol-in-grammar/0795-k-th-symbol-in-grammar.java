37f406b01464280529a6c6cc0241fb5dccf8cf0d
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2023-10-25 07:19:51 +0530

    Time: 0 ms (100%), Space: 39 MB (56.73%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
@@ -3,7 +3,7 @@ class Solution {
         if(n == 1){
             return 0;
         }
-        int length = 1 << (n - 2);
+        int length = 1 << (n - 2); // 2^(n - 2)
         if(k <= length){
             return kthGrammar(n - 1, k);
         }else{
