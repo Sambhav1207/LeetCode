c1570419b1c07410d130bc264cd677f6aff4ff4f
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-05 18:45:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (42.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,20 +4,39 @@ class Solution {
         // Arrays.sort(sortColors);
 
         //Better
-        int count0 = 0, count1 = 0, count2 = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0) count0++;
-            else if(nums[i] == 1) count1++;
-            else count2++;
-        }
-        for(int i = 0; i < count0; i++){
-            nums[i] = 0;
-        }        
-        for(int i = count0; i < count0 + count1; i++){
-            nums[i] = 1;
-        }
-        for(int i = count0 + count1; i < nums.length; i++){
-            nums[i] = 2;
+        // int count0 = 0, count1 = 0, count2 = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] == 0) count0++;
+        //     else if(nums[i] == 1) count1++;
+        //     else count2++;
+        // }
+        // for(int i = 0; i < count0; i++){
+        //     nums[i] = 0;
+        // }        
+        // for(int i = count0; i < count0 + count1; i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i = count0 + count1; i < nums.length; i++){
+        //     nums[i] = 2;
+        // }
+
+        //Optimal: Dutch National Flag Algorithm
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
         }
     }
 }
