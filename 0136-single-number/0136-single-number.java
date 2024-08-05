a43d27f651f0013576d2cd4ba67c448c066bcbc1
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-05 16:23:37 +0530

    Time: 1 ms (99.89%), Space: 46.2 MB (29.65%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -12,6 +12,20 @@ class Solution {
         // }
         // return -1;
 
+        //Better
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i < nums.length; i++){
+        //     max = Math.max(nums[i], max);
+        // }
+        // int[] hash = new int[max + 1];
+        // for(int i = 0; i < nums.length; i++){
+        //     hash[nums[i]]++;
+        // }
+        // for(int i = 0; i < nums.length; i++){
+        //     if(hash[nums[i]] == 1) return nums[i];
+        // }
+        // return -1;
+
         //Optimal
         int xor = 0;
         for(int num : nums){
