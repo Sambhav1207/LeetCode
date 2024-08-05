8300287ea9e75e56bdfb35d30d01b4bbcdc8b317
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-05 18:10:21 +0530

    Time: 5 ms (58.51%), Space: 44.7 MB (57.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -13,7 +13,7 @@ class Solution {
         // }
         // return ans;
 
-        //Better
+        //Better and Optimal for Index Questions
         HashMap<Integer, Integer> map = new HashMap<>();
         int ans[] = new int[]{-1, -1};
         for(int i = 0; i < nums.length; i++){
