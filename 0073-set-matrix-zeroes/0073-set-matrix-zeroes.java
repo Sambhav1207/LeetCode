292e91433e072d7b9617d5d4a12311841e39f24b
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-09 13:55:31 +0530

    Time: 1 ms (80.41%), Space: 45.6 MB (26.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,19 +1,39 @@
 class Solution {
     int temp = (int) 1e9 + 7;
     public void setZeroes(int[][] matrix) {
+        //Brute Force
         int n = matrix.length;
         int m = matrix[0].length;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(matrix[i][j] == 0){
+        //             markRow(matrix, n, m, i);
+        //             markCol(matrix, n, m, j);
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(matrix[i][j] == temp){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        //Better
+        int row[] = new int[n];
+        int col[] = new int[m];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    markRow(matrix, n, m, i);
-                    markCol(matrix, n, m, j);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(matrix[i][j] == temp){
+                if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
