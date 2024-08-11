c63e8b71ac8bc9135332067c9321d1ad4a7f6373
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-11 15:03:59 +0530

    Time: 2 ms (99.37%), Space: 47.8 MB (60.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -20,7 +20,7 @@ class Solution {
         // }
 
         //Better
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
         // for(int i = 0; i < nums.length; i++){
         //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         // }
@@ -32,16 +32,44 @@ class Solution {
         //         break;
         //     }
         // }
-        int min = (int) (nums.length / 3) + 1;
+        // int min = (int) (nums.length / 3) + 1;
+        // for(int i = 0; i < nums.length; i++){
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //     if(map.get(nums[i]) == min){
+        //         list.add(nums[i]);
+        //     }
+        //     if(list.size() == 2){
+        //         break;
+        //     }
+        // }
+
+        //Optimal
+        int count1 = 0, count2 = 0;
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(map.get(nums[i]) == min){
-                list.add(nums[i]);
+            if(count1 == 0 && nums[i] != el2){
+                count1 = 1;
+                el1 = nums[i];
+            }else if(count2 == 0 && nums[i] != el1){
+                count2 = 1;
+                el2 = nums[i];
             }
-            if(list.size() == 2){
-                break;
+            else if(el1 == nums[i]) count1++;
+            else if(el2 == nums[i]) count2++;
+            else{
+                count1--;
+                count2--;
             }
         }
+        count1 = 0; 
+        count2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == el1) count1++;
+            if(nums[i] == el2) count2++;
+        }
+        int min = (int) (nums.length / 3) + 1;
+        if(count1 >= min) list.add(el1);
+        if(count2 >= min) list.add(el2);
         return list;
     }
 }
