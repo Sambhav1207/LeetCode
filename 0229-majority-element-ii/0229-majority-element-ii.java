d7c7b3413406e429d72bbeb53282d480ab613d25
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-11 14:40:12 +0530

    Time: 11 ms (41.63%), Space: 47.6 MB (71.53%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        //Brute Force
         List<Integer> list = new ArrayList<>();
+        //Brute Force
+        // for(int i = 0; i < nums.length; i++){
+        //     if(list.size() == 0 || list.get(0) != nums[i]){
+        //         int count = 0;
+        //         for(int j = 0; j < nums.length; j++){
+        //             if(nums[j] == nums[i]){
+        //                 count++;
+        //             }
+        //         }
+        //         if(count > nums.length / 3){
+        //             list.add(nums[i]);
+        //         }
+        //     }
+        //     if(list.size() == 2){
+        //         break;
+        //     }
+        // }
+
+        //Better
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            if(list.size() == 0 || list.get(0) != nums[i]){
-                int count = 0;
-                for(int j = 0; j < nums.length; j++){
-                    if(nums[j] == nums[i]){
-                        count++;
-                    }
-                }
-                if(count > nums.length / 3){
-                    list.add(nums[i]);
-                }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > nums.length / 3){
+                list.add(entry.getKey());
             }
             if(list.size() == 2){
                 break;
