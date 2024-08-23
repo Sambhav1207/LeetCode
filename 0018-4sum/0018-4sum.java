74dbea05f7cc1e7a18c8dff2d0f28d58ba4bd3a9
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-08-23 18:23:13 +0530

    Time: 472 ms (5.09%), Space: 45.4 MB (22.81%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,34 +1,47 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length - 3; i++){
-            for(int j = i + 1; j < nums.length - 2; j++){
-                long res = (nums[i] + nums[j]);
-                long remaining = target - res;
-                int left = j + 1;
-                int right = nums.length - 1;
-                while(left < right){
-                    long sum = nums[left] + nums[right];
-                    if(sum == remaining){
-                        ArrayList<Integer> temp = new ArrayList<>();
+        int n = nums.length;
+        //Brute Force
+        Set<List<Integer>> set = new HashSet<>();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         for(int k = j + 1; k < n; k++){
+        //             for(int l = k + 1; l < n; l++){
+        //                 long sum = (long) nums[i] + nums[j];
+        //                 sum += nums[k];
+        //                 sum += nums[l];
+        //                 if(sum == target){
+        //                     List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+        //                     Collections.sort(temp);
+        //                     set.add(temp);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+        //Better
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                Set<Long> st = new HashSet<>();
+                for(int k = j + 1; k < n; k++){
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long fourth = target - sum;
+                    if(st.contains(fourth)){
+                        List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
-                        temp.add(nums[left]);
-                        temp.add(nums[right]);
-                        if(!ans.contains(temp)){
-                            ans.add(temp);
-                        }
-                        left++;
-                        right--;
-                    }else if(sum < remaining){
-                        left++;
-                    }else if(sum > remaining){
-                        right--;
+                        temp.add(nums[k]);
+                        temp.add((int) fourth);
+                        temp.sort(Integer::compareTo);
+                        set.add(temp);
                     }
+                    st.add((long) nums[k]);
                 }
             }
         }
+        List<List<Integer>> ans = new ArrayList<>(set);
         return ans;
     }
 }
