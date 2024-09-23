a3de96bb39be6f0ac4b1b25c791f5ece715b01bd
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2024-09-23 08:44:16 +0530

    Time: 44 ms (65.99%), Space: 45.2 MB (75.17%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
@@ -1,16 +1,22 @@
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        for(int i = 1; i <= n; i++){
-            dp[i] = dp[i - 1] + 1;
-            for(String word : dictionary){
-                int len = word.length();
-                if(i >= len && s.substring(i - len, i).equals(word)){
-                    dp[i] = Math.min(dp[i], dp[i - len]);
+        Set<String> set = new HashSet<>();
+        for(String word : dictionary){
+            set.add(word);
+        }
+        int[] arr = new int[s.length() + 1];
+        for(int i = 0; i <= s.length(); i++){
+            arr[i] = s.length();
+        }
+        arr[0] = 0;
+        for(int i = 1; i <= s.length(); i++){
+            arr[i] = arr[i - 1] + 1;
+            for(int j = 0; j < i; j++){
+                if(set.contains(s.substring(j, i))){
+                    arr[i] = Math.min(arr[i], arr[j]);
                 }
             }
         }
-        return dp[n];
+        return arr[s.length()];
     }
 }
