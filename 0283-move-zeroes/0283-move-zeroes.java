f147b756191fea61f8f58b094b7a5286b719f956
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2025-05-05 15:42:42 +0530

    Time: 2 ms (83.91%), Space: 46.1 MB (47.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,17 +2,35 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
         //Brute Force
-        ArrayList<Integer> temp = new ArrayList<>();
+        // ArrayList<Integer> temp = new ArrayList<>();
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] != 0){
+        //         temp.add(nums[i]);
+        //     }
+        // }
+        // for(int i = 0; i < temp.size(); i++){
+        //     nums[i] = temp.get(i);
+        // }
+        // for(int i = temp.size(); i < n; i++){
+        //     nums[i] = 0;
+        // }
+
+        //Optimal
+        int j = -1;
         for(int i = 0; i < n; i++){
-            if(nums[i] != 0){
-                temp.add(nums[i]);
+            if(nums[i] == 0){
+                j = i;
+                break;
             }
         }
-        for(int i = 0; i < temp.size(); i++){
-            nums[i] = temp.get(i);
-        }
-        for(int i = temp.size(); i < n; i++){
-            nums[i] = 0;
+        if(j == -1) return;
+        for(int i = j + 1; i < n; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
         }
     }
 }
