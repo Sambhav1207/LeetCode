5ac39c7abf4a16467a7b8dabef6f3e20679c0eb8
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2025-05-05 22:21:39 +0530

    Time: 1 ms (28.75%), Space: 42.7 MB (8.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,5 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // int i = 0, j = 0;
+        // List<Integer> union = new ArrayList<>();
+        // while(i < m && j < n){
+        //     if(nums1[i] <= nums2[j]){
+        //         if(union.size() == 0 || union.get(union.size() - 1) != nums1[i]){
+        //             union.add(nums1[i]);
+        //         }
+        //         i++;
+        //     }else{
+        //         if(union.size() == 0 || union.get(union.size() - 1) != nums2[j]){
+        //             union.add(nums2[j]);
+        //         }
+        //         j++;
+        //     }
+        // }
+        // while(i < m){
+        //     if(union.get(union.size() - 1) != nums1[i]){
+        //         union.add(nums1[i]);
+        //     }
+        //     i++;
+        // }
+        // while(j < n){
+        //     if(union.get(union.size() - 1) != nums2[j]){
+        //         union.add(nums2[j]);
+        //     }
+        //     j++;
+        // }
+        
         for(int j = 0, i = m; j < n; j++){
             nums1[i] = nums2[j];
             i++;
