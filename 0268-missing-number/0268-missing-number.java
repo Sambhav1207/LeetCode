5d81dbfaa33f740b440bc0a365b9efb47706e50a
Sambhav Gupta <63304958+Sambhav1207@users.noreply.github.com>
2025-05-06 14:39:33 +0530

    Time: 0 ms (100%), Space: 45.9 MB (7.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -13,11 +13,19 @@ class Solution {
 
         //Optimal 1
         int n = nums.length;
-        int sum = (n * (n + 1)) / 2;
-        int s2 = 0;
+        // int sum = (n * (n + 1)) / 2;
+        // int s2 = 0;
+        // for(int i = 0; i < n; i++){
+        //     s2 += nums[i];
+        // }
+        // return (sum - s2);
+
+        //Optimal 2
+        int xor1 = 0, xor2 = 0;
         for(int i = 0; i < n; i++){
-            s2 += nums[i];
+            xor2 ^= nums[i];
+            xor1 ^= (i + 1);
         }
-        return (sum - s2);
+        return (xor1 ^ xor2);
     }
 }
